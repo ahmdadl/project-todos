96be937212fb079a0331ec1aba9c8be4af550545
abo3adel <abo3adel35@gmail.com>
2020-11-28 19:09:36 +0000

    animate hiding the alert

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -2,6 +2,6 @@
     'type' => 'primary',
 ])
 
-<div {!! $attributes->merge(['class' => 'border rounded p-2 m-1 font-bold @if($type==="primary") border-indigo-900 bg-indigo-400 text-indigo-700 @elseif($type==="danger") border-red-900 bg-red-300 text-red-700 @elseif($type==="success") border-greed-800 bg-green-200 text-green-700 @endif']) !!} x-data="{show: true}" x-init="setTimeout(_ => show = false, 3500)" x-show="show">
+<div {!! $attributes->merge(['class' => 'border rounded p-2 m-1 font-bold @if($type==="primary") border-indigo-900 bg-indigo-400 text-indigo-700 @elseif($type==="danger") border-red-900 bg-red-300 text-red-700 @elseif($type==="success") border-greed-800 bg-green-200 text-green-700 @endif']) !!} x-data="{show: true}" x-init="setTimeout(_ => show = false, 3500)" x-show="show" x-transition:enter="transition ease-out duration-500" x-transition:enter-start="opacity-0 transform scale-90" x-transition:enter-end="opacity-100 transform scale-100" x-transition:leave="transition ease-in duration-1000" x-transition:leave-start="opacity-100 transform scale-100" x-transition:leave-end="opacity-0 transform scale-90">
     {{$slot}}
 </div>
