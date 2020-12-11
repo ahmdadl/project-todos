2df1f062fb4910b3e97866c209f970c92fb3ca2a
abo3adel <abo3adel35@gmail.com>
2020-12-11 09:41:39 +0000

    browsersync livewire components

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,4 +19,5 @@ mix.js('resources/js/app.js', 'public/js')
         tailwind('./tailwind.config.js'),
     ])
     .webpackConfig(require('./webpack.config'))
-    .version();
+    .version()
+    .browserSync('js.test');
