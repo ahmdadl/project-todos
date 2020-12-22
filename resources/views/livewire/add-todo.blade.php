048ebc174ff6c120cdcce5d6191b684a78ea9cca
abo3adel <abo3adel35@gmail.com>
2020-12-22 18:41:34 +0000

    styling todo form

--- resources/views/livewire/add-todo.blade.php
+++ resources/views/livewire/add-todo.blade.php
@@ -1,13 +1,13 @@
 <div>
     <div class="mx-auto mb-4 w-3/4">
-        <form wire:submit.prevent='submit' x-data="{body: ''}">
-            <div class="inline w-3/4">
-                <x-jet-input type='text' class='bg-transparent border-gray-600 focus:bg-gray-700 indent-1 w-3/4'
+        <form wire:submit.prevent='submit' x-data="{body: ''}" class=''>
+            <div class="inline">
+                <x-jet-input type='text' class='bg-transparent border-gray-600 focus:bg-gray-700 indent-1 mb-2 w-3/4'
                     placeholder='Add New Task' wire:model.defer='body' x-model='body'
                     x-on:edit-mode.window="body = $event.detail" x-on:edit-mode-off.window="body = ''" />
-                <x-jet-input-error for='body' class='w-3/4 pt-1'></x-jet-input-error>
+                <x-jet-input-error for='body' class='w-3/4 mb-2 pt-1'></x-jet-input-error>
             </div>
-            <div class='my-1'>
+            <div class='inline'>
                 <x-jet-button type='submit'
                     class='bg-green-600 hover:bg-green-900 sm:inline uppercase disabled:bg-red-700' icon='fas fa-save'
                     x-bind:disabled='!body.length || body.length < 5'>
