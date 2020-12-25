d2f1c26dd7a98cff96cc70ada9c57cae899deeab
abo3adel <abo3adel35@gmail.com>
2020-12-25 17:22:56 +0000

    styling navbar

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-gray-900 border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-gray-900 border-b border-gray-900">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-gray-900 text-white">
         <div class="flex justify-between h-16">
