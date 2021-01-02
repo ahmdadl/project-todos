d55b4873257ad4bddc4df45933ae1f6ebea057d5
abo3adel <abo3adel35@gmail.com>
2021-01-02 09:46:32 +0000

    dropdown dark mode

--- resources/views/vendor/jetstream/components/dropdown.blade.php
+++ resources/views/vendor/jetstream/components/dropdown.blade.php
@@ -1,4 +1,4 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white'])
+@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-gray-700 text-white'])
 
 @php
 switch ($align) {
