e603cce1ca66e4ca3ddce15f5276182096061ef4
abo3adel <abo3adel35@gmail.com>
2021-01-04 17:23:41 +0000

    validate category form with alpinejs

--- resources/views/livewire/category/create.blade.php
+++ resources/views/livewire/category/create.blade.php
@@ -1,15 +1,25 @@
 <div>
-    <form class='grid grid-cols-1 sm:grid-cols-3 gap-1 sm:gap-3' wire:submit.prevent='submit'>
-        <div class='sm:col-span-2' x-data="{title: @entangle('title').defer}">
+    <form class='grid grid-cols-1 sm:grid-cols-3 gap-1 sm:gap-3' wire:submit.prevent='submit' x-data="{title: @entangle('title').defer}">
+        <div class='sm:col-span-2'>
             <x-jet-input class='form-input w-full' name='title' wire:model.defer='title' x-model='title'
                 placeholder='Category Title' />
-            <div class='mt-1'>
-                <x-jet-input-error for='title'></x-jet-input-error>
+            <div class='mt-1 font-semibold'>
+                <template x-if='!title.length'>
+                    <x-jet-input-error for='title'></x-jet-input-error>
+                </template>
+                <i></i>
+                <template x-if='title.length > 1 && title.length < 5'>
+                    <p class='text-red-700 dark:text-red-400'>
+                        {{__('validation.min.string', [
+                            'attribute' => 'Title',
+                            'min' => 5
+                        ])}}
+                    </p>
+                </template>
             </div>
         </div>
         <div class='mt-1'>
-            <x-jet-button :bg="$editMode ? 'indigo' : 'green'"
-                type='submit' icon='fas fa-save'>
+            <x-jet-button :bg="$editMode ? 'indigo' : 'green'" type='submit' icon='fas fa-save' x-bind:disabled='!title.length || title.length < 5'>
                 {{ $editMode ? 'update' : 'save' }}
             </x-jet-button>
             <x-jet-button bg='orange' wire:click.prevent='resetProps' icon='fas fa-times'>Reset</x-jet-button>
