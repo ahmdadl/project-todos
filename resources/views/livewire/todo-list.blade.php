c66a14d95e3fa2ae8e06742c3d4545cc63d977ba
abo3adel <abo3adel35@gmail.com>
2021-01-05 19:38:14 +0000

    complete todo if use clicked on body

--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -2,13 +2,13 @@
     <div class="w-3/4 mx-auto rounded shadow mb-2 bg-gray-300 dark:bg-gray-800">
         <div class='grid grid-cols-1 sm:grid-cols-2 font-semibold'>
             <div x-data="{completed: {{ $todo->completed? 1 : 0 }} }" class='px-2 py-2'>
-                <x-jet-button border=''
+            <x-jet-button border='' x-ref="check{{$todo->id}}"
                     class='bg-transparent py-1 px-0 border-4 hover:border-white inline-block hover:text-white transition-all text-sm'
                     x-bind:class="{'border-red-500 text-red-600 hover:bg-red-600': completed, 'border-green-500 text-green-600 hover:bg-green-600': !completed}"
                     icon='fas fa-check' wire:click.prevent='check' target='check'>
                 </x-jet-button>
                 <span
-                    class="{{ $todo->completed ? 'line-through text-green-500' : '' }} inline-block">
+                    class="{{ $todo->completed ? 'line-through text-green-500' : '' }} inline-block hover:cursor-pointer" x-on:click="$refs['check{{$todo->id}}'] ? $refs['check{{$todo->id}}'].click() : false">
                     {{ $todo->body }}
                 </span>
             </div>
