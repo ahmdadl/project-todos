7197ecc8c2aaf67e5ebc5a58df5532ad445f96ab
abo3adel <abo3adel35@gmail.com>
2021-01-06 18:36:21 +0000

    broadcast through ably

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,10 +24,9 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    // wsHost: window.location.hostname,
-    // wsPort: 6001,
-    // forceTLS: false,
-    // disableStats: true,
+    key: process.env.MIX_ABLY_PUBLIC_KEY,
+    wsHost: 'realtime-pusher.ably.io',
+    wsPort: 443,
+    disableStats: true,
+    encrypted: true,
 });
--- resources/views/livewire/todo/create.blade.php
+++ resources/views/livewire/todo/create.blade.php
@@ -40,7 +40,7 @@
                         cancel
                     </x-jet-button>
                 @endif
-                <p class="text-base text-green-300 py-1" x-show='name.length'
+                <p class="text-base text-green-800 dark:text-green-300 py-1" x-show='name.length'
                     x-transition:enter="transition ease-out duration-300"
                     x-transition:enter-start="opacity-0 transform scale-90"
                     x-transition:enter-end="opacity-100 transform scale-100"
