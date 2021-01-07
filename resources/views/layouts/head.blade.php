9149e2f48a48744e1c5bb89cdacbe25311a2d7b7
abo3adel <abo3adel35@gmail.com>
2021-01-07 14:20:14 +0000

    enable content compression through gzip

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
 <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="preload" as='style'>
 
 <script>
     function prefersDark() {
