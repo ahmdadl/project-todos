367cdcc9ee292d964a6789c3e57e67a3bab2c9c9
abo3adel <abo3adel35@gmail.com>
2021-01-07 12:27:22 +0000

    broadcast through pusher

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,9 +10,7 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_ABLY_PUBLIC_KEY,
-    wsHost: 'realtime-pusher.ably.io',
-    wsPort: 443,
-    disableStats: true,
-    encrypted: true
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    forceTLS: true
 });
