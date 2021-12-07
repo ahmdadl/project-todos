1a3c55c64a5cf80f2728c73242b4510c294969c3
abo3adel <abo3adel35@gmail.com>
2021-12-07 11:09:37 +0000

    update footer name

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,10 +1,10 @@
-<div class='px-2 py-5 bg-gray-900 dark:bg-blue-900 dark:border-t dark:border-gray-100 text-white'>
-    <div class='grid grid-cols-1 sm:grid-cols-2 gap-3 sm:gap-2'>
+<div class='px-2 py-5 text-white bg-gray-900 dark:bg-blue-900 dark:border-t dark:border-gray-100' dir="ltr">
+    <div class='grid grid-cols-1 gap-3 sm:grid-cols-2 sm:gap-2'>
         @include('footer.social')
         <div class='text-center'>
             Copyright <a href="https://abo3adel.github.io/" target='_blank'
-                class="text-yellow-400 text-lg whitespace-pre hover:font-bold"
-                style="font-variant: small-caps">NinjaCoder</a>&copy; 2020
+                class="text-lg text-yellow-400 whitespace-pre hover:font-bold"
+                style="font-variant: small-caps">Ahmed Adel</a>&copy; 2020
             {{ date('Y') > 2020 ? '- ' . date('Y') : '' }}.
             <br />
             <span class='inline-block'>All rights reserved</span>
